34cd76a5e23ed17198dcee5b9cac9e86ef63dc94
yepsamii <abrahmansw@gmail.com>
2026-06-04 18:34:38 +0600

    add dotenv

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,24 +1,24 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectDB = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    }
-    catch (error ){
-        console.log (error);
-        process.exit(1);
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
 export const disconnectDB = async () => {
-    try{
-        await mongoose.connection.close();
-        console.log("MongoDB Disconnected");
-        process.exit(0);
-    }
-    catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connection.close();
+    console.log("MongoDB Disconnected");
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
